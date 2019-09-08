347f56e655ce7875678d982201539962c84376bf
Diko Parvanov <diko.parvanov@canonical.com>
2019-09-08 14:29:23 +0300

    Added multiple networks checks + MTU config option

--- reactive/prometheus-blackbox-exporter.py
+++ reactive/prometheus-blackbox-exporter.py
@@ -1,3 +1,4 @@
+import ast
 import subprocess
 import yaml
 
@@ -6,6 +7,8 @@ from charmhelpers.core.templating import render
 from charms.reactive import (
     when, when_not, set_state, remove_state
 )
+from ipaddress import IPv4Interface
+from netifaces import interfaces, ifaddresses, AF_INET
 from charms.reactive.helpers import any_file_changed, data_changed
 from charms.reactive import hook
 
@@ -92,34 +95,65 @@ def restart_blackbox_exporter():
     set_state('blackbox-exporter.started')
     remove_state('blackbox-exporter.do-restart')
 
+def get_module():
+    # TODO(dparv): rewrite to query the principal service and provide
+    # reasonable modules to probe - http/https/ssl certs/ssh/icmp/etc.
+    return 'icmp'
 
 # Relations
-@hook('blackbox-peer-relation-{joined,changed}','blackbox-exporter-relation-{joined,changed}')
+@hook('blackbox-peer-relation-{joined,changed,departed}','blackbox-exporter-relation-{joined,changed}')
 def configure_blackbox_exporter_relation(peers):
     config = hookenv.config()
 
     targets = []
+    networks = []
     for rid in hookenv.relation_ids('blackbox-peer'):
         for unit in hookenv.related_units(rid):
-            addr = hookenv.relation_get('private-address', rid=rid, unit=unit)
             principal_unit = hookenv.relation_get('principal-unit', rid=rid, unit=unit)
-            targets.append({'ip-address': addr, 'principal-unit': principal_unit})
+            unit_networks = hookenv.relation_get('unit-networks', rid=rid, unit=unit)
+            if unit_networks is None:
+                return
+            unit_networks = ast.literal_eval(unit_networks)
+            for unit_network in unit_networks:
+                # Chcek if same network exists on this unit
+                if unit_network['net'] in [net['net'] for net in get_unit_networks()]:
+                    networks.append(unit_network['net'])
+                    targets.append({
+                        'network': unit_network['net'],
+                        'interface': unit_network['iface'],
+                        'ip-address': unit_network['ip'],
+                        'principal-unit': principal_unit,
+                        })
 
     relation_settings = {}
     relation_settings['targets'] = targets
+    relation_settings['networks'] = networks
     relation_settings['ip_address'] = hookenv.unit_get('private-address')
     relation_settings['port'] = PORT_DEF
     relation_settings['job_name'] = hookenv.principal_unit()
     relation_settings['module'] = get_module()
     relation_settings['scrape_interval'] = config.get('scrape-interval')
 
+    tcp_probes = []
+    relation_settings['tcp_probes'] = tcp_probes
+
     for rel_id in hookenv.relation_ids('blackbox-exporter'):
         hookenv.relation_set(relation_id=rel_id, relation_settings=relation_settings)
 
-def get_module():
-    # TODO(dparv): rewrite to query the principal service and provide
-    # reasonable modules to probe - http/https/ssl certs/ssh/icmp/etc.
-    return 'icmp'
+def get_unit_networks():
+    networks = []
+    for iface in interfaces():
+        if iface == 'lo': continue
+        ip_addresses = ifaddresses(iface)
+        if AF_INET in ip_addresses:
+            for ip_address in ip_addresses[AF_INET]:
+                ip_v4 = IPv4Interface("{}/{}".format(ip_address['addr'], ip_address['netmask']))
+                networks.append({"iface": iface, "ip": ip_v4.ip.__str__(), "net": ip_v4.network.__str__()})
+    return networks
+
+def get_opened_ports():
+    ports = hookenv.opened_ports()
+    return [port.split('/')[0] for port in ports]
 
 @when('blackbox-peer.connected')
 def setup_blackbox_peer_relation(peers):
@@ -128,5 +162,6 @@ def setup_blackbox_peer_relation(peers):
         relation_settings = hookenv.relation_get(rid=rid, unit=hookenv.local_unit())
         relation_settings['principal-unit'] = hookenv.principal_unit()
         relation_settings['private-address'] = hookenv.unit_get('private-address')
+        relation_settings['unit-networks'] = get_unit_networks()
         hookenv.relation_set(relation_id=rid, relation_settings=relation_settings)
 
